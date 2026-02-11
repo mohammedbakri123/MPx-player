53f305e14ee73bbdd0b799ab6a3650c12bd58d43
mohammed <mohaa.b.4321@gmail.com>
2026-02-11 11:11:52 +0300

    just commented last_played_service.dart i will finish it later

--- lib/core/services/last_played_service.dart
+++ lib/core/services/last_played_service.dart
@@ -1,37 +1,39 @@
-import 'package:shared_preferences/shared_preferences.dart';
-import '../models/last_played_video.dart';
+///TODO: Implement a service to manage the last played video using shared_preferences.
 
-class LastPlayedService {
-  static const String _lastPlayedVideoKey = 'last_played_video';
+// import 'package:shared_preferences/shared_preferences.dart';
+// import '../models/last_played_video.dart';
 
-  static late SharedPreferences _prefs;
+// class LastPlayedService {
+//   static const String _lastPlayedVideoKey = 'last_played_video';
 
-  static Future<void> init() async {
-    _prefs = await SharedPreferences.getInstance();
-  }
+//   static late SharedPreferences _prefs;
 
-  // Save the last played video
-  static Future<bool> saveLastPlayedVideo(LastPlayedVideo video) async {
-    final jsonString = video.toJson();
-    return await _prefs.setString(_lastPlayedVideoKey, jsonString);
-  }
+//   static Future<void> init() async {
+//     _prefs = await SharedPreferences.getInstance();
+//   }
 
-  // Get the last played video
-  static LastPlayedVideo? getLastPlayedVideo() {
-    final jsonString = _prefs.getString(_lastPlayedVideoKey);
-    if (jsonString != null) {
-      return LastPlayedVideo.fromJson(jsonString);
-    }
-    return null;
-  }
+//   // Save the last played video
+//   static Future<bool> saveLastPlayedVideo(LastPlayedVideo video) async {
+//     final jsonString = video.toJson();
+//     return await _prefs.setString(_lastPlayedVideoKey, jsonString);
+//   }
 
-  // Clear the last played video
-  static Future<bool> clearLastPlayedVideo() async {
-    return await _prefs.remove(_lastPlayedVideoKey);
-  }
+//   // Get the last played video
+//   static LastPlayedVideo? getLastPlayedVideo() {
+//     final jsonString = _prefs.getString(_lastPlayedVideoKey);
+//     if (jsonString != null) {
+//       return LastPlayedVideo.fromJson(jsonString);
+//     }
+//     return null;
+//   }
 
-  // Check if there's a last played video
-  static bool hasLastPlayedVideo() {
-    return _prefs.getString(_lastPlayedVideoKey) != null;
-  }
-}
+//   // Clear the last played video
+//   static Future<bool> clearLastPlayedVideo() async {
+//     return await _prefs.remove(_lastPlayedVideoKey);
+//   }
+
+//   // Check if there's a last played video
+//   static bool hasLastPlayedVideo() {
+//     return _prefs.getString(_lastPlayedVideoKey) != null;
+//   }
+// }
